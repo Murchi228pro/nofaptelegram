a7701188bc1f51666595034e031421487eb02fca
Murchi228pro <myt021007@gmail.com>
2024-10-12 15:45:27 +0300

    Тест бота

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 from aiogram import Dispatcher, Bot
+from aiogram.types import Message
+import asyncio
+from aiogram.filters import CommandStart
+
+from config import TOKEN
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart)
+async def start(message: Message):
+    await message.reply(f"Hello))))")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
